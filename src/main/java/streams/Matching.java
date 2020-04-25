995f184bd319f49acbd07ebf57199789a7427db7
wangxu <1609433522@qq.com>
2020-04-25 10:58:27 +0800

    cimmit

--- src/main/java/streams/Matching.java
+++ src/main/java/streams/Matching.java
@@ -3,30 +3,34 @@ package streams;// streams/Matching.java
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
 // Demonstrates short-circuiting of *Match() operations
+
 import java.util.stream.*;
 import java.util.function.*;
+
 import static streams.RandInts.*;
 
 interface Matcher extends
-  BiPredicate<Stream<Integer>, Predicate<Integer>> {}
+        BiPredicate<Stream<Integer>, Predicate<Integer>> {
+}
 
 public class Matching {
-  static void show(Matcher match, int val) {
-    System.out.println(
-      match.test(
-        IntStream.rangeClosed(1, 9)
-          .boxed()
-          .peek(n -> System.out.format("%d ", n)),
-        n -> n < val));
-  }
-  public static void main(String[] args) {
-    show(Stream::allMatch, 10);
-    show(Stream::allMatch, 4);
-    show(Stream::anyMatch, 2);
-    show(Stream::anyMatch, 0);
-    show(Stream::noneMatch, 5);
-    show(Stream::noneMatch, 0);
-  }
+    static void show(Matcher match, int val) {
+        System.out.println(
+                match.test(
+                        IntStream.rangeClosed(1, 9)
+                                .boxed()
+                                .peek(n -> System.out.format("%d ", n)),
+                        n -> n < val));
+    }
+
+    public static void main(String[] args) {
+        show(Stream::allMatch, 10);
+        show(Stream::allMatch, 4);
+        show(Stream::anyMatch, 2);
+        show(Stream::anyMatch, 0);
+        show(Stream::noneMatch, 5);
+        show(Stream::noneMatch, 0);
+    }
 }
 /* Output:
 1 2 3 4 5 6 7 8 9 true
--- src/main/java/streams/Reduce.java
+++ src/main/java/streams/Reduce.java
@@ -2,32 +2,39 @@ package streams;// streams/Reduce.java
 // (c)2017 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
+
 import java.util.*;
 import java.util.stream.*;
 
 class Frobnitz {
-  int size;
-  Frobnitz(int sz) { size = sz; }
-  @Override
-  public String toString() {
-    return "Frobnitz(" + size + ")";
-  }
-  // Generator:
-  static Random rand = new Random(47);
-  static final int BOUND = 100;
-  static Frobnitz supply() {
-    return new Frobnitz(rand.nextInt(BOUND));
-  }
+    int size;
+
+    Frobnitz(int sz) {
+        size = sz;
+    }
+
+    @Override
+    public String toString() {
+        return "Frobnitz(" + size + ")";
+    }
+
+    // Generator:
+    static Random rand = new Random(47);
+    static final int BOUND = 100;
+
+    static Frobnitz supply() {
+        return new Frobnitz(rand.nextInt(BOUND));
+    }
 }
 
 public class Reduce {
-  public static void main(String[] args) {
-    Stream.generate(Frobnitz::supply)
-      .limit(10)
-      .peek(System.out::println)
-      .reduce((fr0, fr1) -> fr0.size < 50 ? fr0 : fr1)
-      .ifPresent(System.out::println);
-  }
+    public static void main(String[] args) {
+        Stream.generate(Frobnitz::supply)
+                .limit(10)
+                .peek(System.out::println)
+                .reduce((fr0, fr1) -> fr0.size < 50 ? fr0 : fr1)
+                .ifPresent(System.out::println);
+    }
 }
 /* Output:
 Frobnitz(58)
